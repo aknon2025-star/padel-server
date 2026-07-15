a91c0a6472031e60e08bc313fb39f3d7c4275b46
aknon2025-star <aknon2025@gmail.com>
2026-07-15 16:10:42 +0330

    Update tournaments.js

--- tournaments.js
+++ tournaments.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 const { v4: uuid } = require("uuid");
-const { db } = require("../db/database");
+const db = require("./database");
 const { awardPoints } = require("./users");
 const router = express.Router();
 
