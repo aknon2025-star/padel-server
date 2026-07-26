5ffad1071fcb0c11d3ba10dcd0d56158a407e9e3
aknon2025-star <aknon2025@gmail.com>
2026-07-26 12:57:43 +0330

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 // db/database.js
 // استفاده از دیتابیس اصلی پروژه و جلوگیری از ساخت دیتابیس دوم
 
-const { db } = require("../database");
+const db = require("../database");
 
 module.exports = db;
